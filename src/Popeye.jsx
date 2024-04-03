b0516a9da819c9af0704d97e7fd44a217e645672
kevin5112 <kevin5112@yahoo.com>
2024-04-03 12:59:34 -0700

    render components and multi page layouts without refreshing

---
+++ src/Popeye.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Popeye = () => {
+  return (
+    <>
+      <p>Hi, I am Popeye! I love to eat Spinach!</p>
+      <Link to="/">Clik here to go back</Link>
+    </>
+  );
+};
+
+export default Popeye;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import DefaultProfile from './DefaultProfile.jsx';
+import './index.css';
+import Popeye from './Popeye.jsx';
+import Profile from './Profile.jsx';
+import Spinach from './Spinach.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: 'profile',
+    element: <Profile />,
+    children: [
+      {
+        index: true,
+        element: <DefaultProfile />,
+      },
+      {
+        path: 'popeye',
+        element: <Popeye />,
+      },
+      {
+        path: 'spinach',
+        element: <Spinach />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
